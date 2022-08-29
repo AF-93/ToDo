c581eb26e63458f121d840b66f8a800cc986c9d1
Alan Fernandez <fernandezalan93@gmail.com>
2022-08-29 09:56:10 -0300

    finalizando clase 3

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 const btn = document.querySelector("[data-form-btn]");
 
+
+
 const createTask = (evento)=>{
     evento.preventDefault();
     const input = document.querySelector("[data-form-input]")
+    const list = document.querySelector("[data-list]")
+    const task = document.createElement("li")
+    task.classList.add("card")
     inputV = input.value
-    console.log(inputV); 
+    input.value = ""
+    const content = `<div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${inputV}</span>
+  </div>
+  <i class="fas fa-trash-alt trashIcon icon"></i>
+</li>`;
+
+    task.innerHTML=content
+
+    list.appendChild(task);
+
+    console.log("todo normal aqui amiguito"); 
 };
 
 btn.addEventListener("click", createTask);
